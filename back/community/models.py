8a497db2f68423d807cd696bd92e71efa2cc357b
yslgitt <ysltub@gmail.com>
2022-06-16 23:36:07 +0900

    merge

--- back/community/models.py
+++ back/community/models.py
@@ -6,11 +6,11 @@ from forests.models import Forest
 class Article(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField(null=True)
-    using_date = models.DateField(null=True)
+    # using_date = models.DateField(null=True)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    forest = models.ForeignKey(Forest, on_delete=models.CASCADE)
+    # forest = models.ForeignKey(Forest, on_delete=models.CASCADE)
     article_image = models.ImageField(upload_to='article/', null=True)
 
 
