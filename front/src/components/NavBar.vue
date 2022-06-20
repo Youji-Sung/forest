8c44fc18493769940ab5bdbcb124941d202c9885
{mindot7} <{codemj11@gmail.com}>
2022-06-21 00:37:31 +0900

    [docs]NavBar router 추가

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -1,8 +1,9 @@
 <template>
   <nav>
     NavBar 자리 입니다
-    <router-link to="/">Home</router-link> |
-    <router-link>Login</router-link>
+    <router-link to="/">홈</router-link> |
+    <router-link to="/login">로그인</router-link> |
+    <router-link to="/profile">프로필</router-link>
   </nav>
 </template>
 
