1486036ca9f19ee9d7cf2ad356c443be45447302
성유지 <anc9432@gmail.com>
2022-06-20 15:56:42 +0900

    [docs] accounts views & components design

---
+++ front/src/views/accounts/LoginView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+
+    <div class="row text-center" style="width: 101% color: rgb(0, 117, 149);">
+      <h1 class="mt-3">로그인</h1>
+      <account-error-list v-if="authError"></account-error-list>
+    </div>
+
+    <div class="d-flex justify-content-center mx-5 mb-5">
+      <div class="d-flex justify-content-center">
+        <form @submit.prevent="login(credentials)">
+
+          <div>
+            <label for="username" class="d-flex justify-content-center mt-3" style="color: rgb(0, 117, 149);">아이디 </label>
+            <input v-model="credentials.username" type="text" id="username" required />
+          </div>
+
+          <div>
+            <label for="password" class="d-flex justify-content-center mt-3" style="color: rgb(0, 117, 149);">비밀번호 </label>
+            <input v-model="credentials.password" type="password" id="password" required />
+          </div>
+          
+          <div class="d-flex justify-content-center mt-3">
+            <button class="btn btn-outline-info" style="color: rgb(0, 117, 149);">Login</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+  import { mapActions, mapGetters } from 'vuex'
+  import AccountErrorList from '@/components/accounts/AccountErrorList.vue'
+
+  export default {
+    name: 'LoginView',
+    components: {
+      AccountErrorList,
+    },
+    data() {
+      return {
+        credentials: {
+          username: '',
+          password: '',
+        }
+      }
+    },
+    computed: {
+      ...mapGetters(['authError'])
+    },
+    methods: {
+      ...mapActions(['login'])
+    },
+  }
+</script>
+
+<style></style>
---
+++ front/src/views/accounts/ProfileView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="row text-center" style="width: 101%">
+      <h1 class="my-3 mb-4" >{{ profile.username }}님의 프로필</h1>
+      <h5 class="my-3" style="color: rgb(0, 117, 149);">Email address</h5>
+      <span class="mb-5">{{ profile.email }}</span>
+    </div>
+
+    <div class="d-flex justify-content-center mx-5 mb-5">
+      <div class="d-flex box justify-content-center">
+        <img :src="imgUrl" alt="a" class="profile">
+      </div>
+    </div>
+
+    <div class="d-flex justify-content-center mx-5 mb-5">
+      <button type="button" class="btn btn-outline-info" style="color: rgb(0, 117, 149);">수정하기</button>
+    </div>
+
+    <div class="d-flex justify-content-center mx-5 mb-5">
+      <div class="d-flex box justify-content-center">
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'ProfileView',
+  computed: {
+    ...mapGetters(['profile']),
+    imgUrl(){
+      // return 값 앞에 장고 서버를 붙여줘야 함/그리고 사이즈 조정
+      return `http://127.0.0.1:8000${this.profile.user_profile}`
+    }
+  },
+  methods: {
+    ...mapActions(['fetchProfile'])
+  },
+  created() {
+    const payload = { username: this.$route.params.username }
+    this.fetchProfile(payload)
+  },
+}
+</script>
+
+<style scoped>
+.box {
+    width: 300px;
+    height: 300px; 
+    border-radius: 100%;
+    overflow: hidden;
+}
+.profile {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
