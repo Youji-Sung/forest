a75105f2a8441fd4dad2322b81d865f656e764fb
{mindot7} <{codemj11@gmail.com}>
2022-06-21 23:51:27 +0900

    0621mj

---
+++ front/src/components/home/ForestCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h5>휴양림 정보</h5>
+    <p>{{ cardName }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ForestCard',
+  props: {
+    card: {
+      type: Object
+    }
+  },
+  computed: {
+    cardContext: function() {
+      const context = this.card.fields
+      return context
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
