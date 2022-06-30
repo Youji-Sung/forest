ba0820c972bb34b359e84210158d009a04a4e97a
성유지 <anc9432@gmail.com>
2022-06-30 23:52:48 +0900

    [docs] navbar

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -1,10 +1,31 @@
 <template>
-  <nav>
-    NavBar 자리 입니다
-    <router-link to="/">홈</router-link> |
-    <router-link to="/login">로그인</router-link> |
-    <router-link to="/profile/:username">프로필</router-link> |
-    <router-link to="/forests">휴양림</router-link>
+  <nav class="navbar navbar-expand-sm navbar-light bg-white">
+    <div class="collapse navbar-collapse m-3">
+      <router-link to="/" class="navbar-brand">
+        <img src="../assets/forests.jpg" alt="logo" class="logo" style="height:80px;" />
+      </router-link>
+      <ul class="navbar-nav">
+        <!-- <li class="nav-item m-md-3">
+          <router-link :to="{ name: 'home' }" class="nav-link" style="color: gray;">홈</router-link>
+        </li> -->
+        <li class="nav-item m-md-3" >
+          <router-link :to="{ name: 'forests' }" class="nav-link" style="color: gray;">휴양림</router-link>
+        </li>
+        <li class="nav-item m-md-3">
+          <router-link v-if="isLoggedIn" :to="{ name: 'logout' }" class="nav-link " style="color: gray;">Logout</router-link>
+        </li>
+        <li class="nav-item m-md-3">
+          <router-link v-if="isLoggedIn" :to="{ name: 'profile', params: { username } }" class="nav-link">
+          {{ currentUser.username }}님의 프로필</router-link>
+        </li>
+        <li class="nav-item m-md-3">
+          <router-link v-if="!isLoggedIn" :to="{ name: 'login' }" class="nav-link" style="color: gray;">Login</router-link>
+        </li>
+        <li class="nav-item m-md-3">
+          <router-link v-if="!isLoggedIn" :to="{ name: 'signup' }" class="nav-link" style="color: gray;">Signup</router-link>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
@@ -15,5 +36,16 @@ export default {
 </script>
 
 <style>
+header {
+  width: 80%;      
+  margin: 0 auto;   
+  padding: 1rem 0; 
+}
 
+.profile {
+    height:60px;
+    width:60px;
+    object-fit: cover;
+    border-radius: 100%;
+}
 </style>
