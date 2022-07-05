b0acc4cd10ac6dff78968b028d0bd3c6c2bbf0a7
성유지 <anc9432@gmail.com>
2022-07-06 00:04:01 +0900

    [docs] commentlistform

--- front/src/components/communities/CommentListForm.vue
+++ front/src/components/communities/CommentListForm.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-
+    <form @submit.prevent="onSubmit">
+      <input type="text" id="comment" v-model="content" required>
+      <button type="submit">댓글 달기</button>
+    </form>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
-  name: 'CommentListForm'
+  name: 'CommentListForm',
+  data() {
+    return {
+      content: ''
+    }
+  },
+  computed: {
+    ...mapGetters(['article'])
+  },
+  methods: {
+    ...mapActions(['createArticleComment']),
+    onSubmit() {
+      // pk일까 id일까
+      this.createArticleComment({ articlePk: this.article.pk, content: this.content })
+      this.content = ''
+    }
+  }
 }
 </script>
 
