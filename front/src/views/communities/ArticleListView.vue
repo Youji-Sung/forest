a91e4f9744b37b96894f9f57d51c0253a723f6fb
성유지 <anc9432@gmail.com>
2022-07-05 23:26:53 +0900

    [docs] articlelistview

--- front/src/views/communities/ArticleListView.vue
+++ front/src/views/communities/ArticleListView.vue
@@ -1,12 +1,37 @@
 <template>
   <div>
-    
+    <router-link :to="{ name: 'articleNew' }">
+      <button>새 글 쓰기</button>
+    </router-link>
+
+    <ul v-for="article in articles" :key="article.pk">
+      <li v-if="article.user">
+        {{ article.user.username }}
+        <div>
+          <router-link :to="{ name: 'article', params: {articlePk: article.pk} }">
+            {{ article.title }}
+          </router-link>
+        </div>
+      </li>
+    </ul>
+
   </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+
 export default {
-  name: 'ArticleList'
+  name: 'ArticleList',
+  computed: {
+    ...mapGetters(['articles'])
+  },
+  methods: {
+    ...mapActions(['fetchArticles'])
+  },
+  created() {
+    this.fetchArticles()
+  }
 }
 </script>
 
