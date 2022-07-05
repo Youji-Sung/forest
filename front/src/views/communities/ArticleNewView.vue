c4d72ef010ef78e6fb747909a9a001a89306574d
성유지 <anc9432@gmail.com>
2022-07-05 23:29:18 +0900

    [docs] articlenewview

--- front/src/views/communities/ArticleNewView.vue
+++ front/src/views/communities/ArticleNewView.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    
+    <article-form :article="article" action='create'>
+      <button>새 글 쓰기</button>
+    </article-form>
   </div>
 </template>
 
 <script>
+import ArticleForm from '@/components/communities/ArticleForm.vue'
+
 export default {
-  name: 'ArticleNew'
+  name: 'ArticleNew',
+  components: { ArticleForm },
+  data() {
+    return {
+      article: {
+        pk: null,
+        title: '',
+        content: '',
+        article_image: '',
+      }
+    }
+  }
 }
 </script>
 
